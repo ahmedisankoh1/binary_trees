876cbfc096f2e969c0f5a58927b0b807b99115c9
Ahmed I Sankoh <ahmedisankoh1@gmail.com>
2024-02-28 16:17:06 -0800

    hi

--- binary_trees.h
+++ binary_trees.h
@@ -26,3 +26,6 @@ typedef struct binary_tree_s heap_t;
 
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif
